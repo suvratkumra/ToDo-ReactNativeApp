a0487ebf406facb6bf515356d1b95cf2cd3e3440
Suvrat Kumra <59862013+suvratkumra@users.noreply.github.com>
2023-07-28 09:40:24 -0400

    feat: Assigning unique device id to every mobile device who downloads the app

--- ToDoApp/components/Home.js
+++ ToDoApp/components/Home.js
@@ -2,18 +2,46 @@ import { useEffect, useState } from "react";
 import { Alert, View, Text, StyleSheet, Button, TouchableOpacity, TextInput } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// all these imports for unique id for the device.
+import * as SecureStore from 'expo-secure-store'
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+
 function HomeScreen({ navigation }) {
 
     // extract them from the local storage.
     const [existingLists, setExistingLists] = useState([]);
 
 
+
     function goToPageHandler(name) {
         navigation.navigate(name)
     }
 
+    useEffect(() => {
+        // calling this function to assign a unique id to the phone.
+        const deviceUUID = async () => {
+            let uuid = uuidv4();
+            let fetchUUID = await SecureStore.getItemAsync('secure_deviceid');
+            //if user has already signed up prior
+            if (fetchUUID) {
+                uuid = fetchUUID
+            }
+            else {
+                await SecureStore.setItemAsync('secure_deviceid', JSON.stringify(uuid));
+            }
+            console.log(uuid)
+        }
+        const deletePreviousUUID = async () => {
+            await SecureStore.deleteItemAsync('secure_deviceid');
+        }
+        deviceUUID();
+        // deletePreviousUUID();
+    }, [])
+
     return (
         <View style={styles.container}>
+
             <View style={styles.newListContainer}>
                 <TouchableOpacity onPress={() => { navigation.navigate('NewList') }}>
                     <View style={styles.linkContainer}>
